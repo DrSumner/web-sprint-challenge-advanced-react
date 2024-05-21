4731780c189eadfa9a217586df6a0ac3fff1baaa
DrSumner <157843168+DrSumner@users.noreply.github.com>
2024-05-21 13:28:27 -0400

    finito

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,70 @@
-// Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
+ // Write your tests here
+ import React from 'react'
+ import { render, fireEvent, screen, waitFor } from '@testing-library/react'
+ import '@testing-library/jest-dom/extend-expect'
+ import AppFunctional from './AppFunctional' 
+
+describe('sanity', () => {
+  let  left, right, up, down, email
+
+  beforeEach(() => {
+
+    render(<AppFunctional/>)
+    //user = userEvent.setup()
+    left = screen.getByText('LEFT')
+    right = screen.getByText('RIGHT')
+    up = screen.getByText('UP')
+    down = screen.getByText('DOWN')
+    email = document.querySelector('#email')
+  })
+
+  test('[1] Grid works as intended', async () => {
+    screen.debug()
+
+    await fireEvent.click(left)
+    await fireEvent.click(left)
+    expect(screen.getByText("You can't go left"))
+  })
+
+  test('[2] Grid works as intended', async () => {
+    screen.debug()
+
+    await fireEvent.click(up)
+    await fireEvent.click(right)
+    expect(screen.getByText("You moved 2 times"))
+    expect(screen.getByText("Coordinates (3, 1)"))
+  })
+  test('[3] Coordinates are correct', async () => {
+    screen.debug()
+
+    await fireEvent.click(up)
+    await fireEvent.click(up)
+    await fireEvent.click(left)
+    expect(screen.getByText("Coordinates (1, 1)"))
+  })
+  test('[4] Steps works as intended', async () => {
+    screen.debug()
+
+    await fireEvent.click(down)
+    await fireEvent.click(down)
+    await fireEvent.click(down)
+    await fireEvent.click(down)
+    await fireEvent.click(left)
+    expect(screen.getByText("Coordinates (1, 3)"))
+    expect(screen.getByText("You moved 2 times"))
+  })
+  test('[5] App is functional', async () => {
+    screen.debug()
+
+    await fireEvent.click(up)
+    await fireEvent.click(left)
+    await fireEvent.click(down)
+    await fireEvent.click(right)
+    fireEvent.change(email, { target: { value: 'tre@diff.com' } })
+    
+    expect(screen.getByText("Coordinates (2, 2)"))
+    expect(screen.getByText("You moved 4 times"))
+    expect(screen.findByText("tre win #72"))
+  })
+
 })
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,77 +1,139 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 // Suggested initial states
 const initialMessage = ''
 const initialEmail = ''
 const initialSteps = 0
 const initialIndex = 4 // the index the "B" is at
+const initialValues = { x: 2, y: 2, steps: 0, email: "" }
+const test = [
+  [1,2,4,5,7,8],
+  [0,1,3,4,6,7],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+]
 
 export default function AppFunctional(props) {
-  // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
-  // You can delete them and build your own logic from scratch.
+ const [coord, setCoord] = useState({x: 2, y: 2})
+ const [values, setValues] = useState(initialValues)
+ const [steps, setSteps] = useState(initialSteps)
+ const [index, setIndex] = useState(initialIndex)
+ const [message, setMessage] = useState(initialMessage)
+ const [email, setEmail] = useState('')
 
-  function getXY() {
-    // It it not necessary to have a state to track the coordinates.
-    // It's enough to know what index the "B" is at, to be able to calculate them.
+ useEffect(() =>{
+  setCoord(
+    {x:test[2].includes(index) ? 1: test[3].includes(index) ? 2 :3,
+      y:test[5].includes(index) ? 1: test[6].includes(index) ? 2:3
+    })
+    setValues({...values, x: coord.x, y: coord.y, steps: steps, })
+ }, [index])
+
+ useEffect(() =>{
+  
+    setValues({...values, x: coord.x, y: coord.y, steps: steps, })
+ }, [coord])
+
+ useEffect(() =>{
+  
+  setValues({ x: coord.x, y: coord.y, steps: steps, email: email })
+}, [email])
+
+  const handleDirection = e => {
+  const {id} = e.target
+  
+  
+  if(id == 'left'){
+    let click = test[0].includes(index) 
+    ? function() {setIndex(index - 1); setSteps(steps +1); setMessage(initialMessage);}
+    : function() {setMessage("You can't go left")}
+    click()
+  }
+if(id == 'right'){
+  let click = test[1].includes(index) 
+    ? function() {setIndex(index + 1); setSteps(steps +1); setMessage(initialMessage)}
+    : function() {setMessage("You can't go right")}
+    click()
+}
+if(id == 'up'){
+  let click = index - 3 >=0 
+  ? function() {setIndex(index - 3); setSteps(steps +1); setMessage(initialMessage)}
+  : function() {setMessage("You can't go up")}
+  click()
+}
+  if(id == 'down'){
+    let click = index + 3 <=8 
+  ? function() {setIndex(index + 3); setSteps(steps +1); setMessage(initialMessage)}
+  : function() {setMessage("You can't go down")}
+  click()
   }
 
-  function getXYMessage() {
-    // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
-    // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
-    // returns the fully constructed string.
+   
+
+ }
+
+  const reset = () => {
+      setCoord({x: 2, y: 2})
+      setValues(initialValues)
+      setSteps(initialSteps)
+      setIndex(initialIndex)
+      setMessage(initialMessage)
+      setEmail('')
   }
 
-  function reset() {
-    // Use this helper to reset all states to their initial values.
+  const handleChanges = e => {
+    const {value} = e.target
+    setEmail(value)
   }
 
-  function getNextIndex(direction) {
-    // This helper takes a direction ("left", "up", etc) and calculates what the next index
-    // of the "B" would be. If the move is impossible because we are at the edge of the grid,
-    // this helper should return the current index unchanged.
-  }
+  const handleSubmit = e => {
+    e.preventDefault()
 
-  function move(evt) {
-    // This event handler can use the helper above to obtain a new index for the "B",
-    // and change any states accordingly.
-  }
+    axios.post('http://localhost:9000/api/result', values)
+    .then( res => {
+      console.log(res); setEmail('') ;setMessage(res.data.message)
+    })
+    .catch( err => {
+      console.log(err.response.data.message);
+        setMessage(err.response.data.message)
+    })
 
-  function onChange(evt) {
-    // You will need this to update the value of the input.
-  }
-
-  function onSubmit(evt) {
-    // Use a POST request to send a payload to the server.
   }
 
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved 0 times</h3>
+        <h3 id="coordinates">Coordinates ({coord.x}, {coord.y})</h3>
+        <h3 id="steps">You moved {steps} {steps === 1 ? 'time': 'times'}</h3>
       </div>
       <div id="grid">
         {
           [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-            <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-              {idx === 4 ? 'B' : null}
+            <div key={idx} className={`square${idx === index ? ' active' : ''}`}>
+              {idx === index ? 'B' : null}
             </div>
           ))
         }
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{message}</h3>
       </div>
       <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
-        <button id="reset">reset</button>
+        <button onClick={handleDirection} id="left">LEFT</button>
+        <button onClick={handleDirection} id="up">UP</button>
+        <button onClick={handleDirection} id="right">RIGHT</button>
+        <button onClick={handleDirection} id="down">DOWN</button>
+        <button onClick={reset} id="reset">reset</button>
       </div>
-      <form>
-        <input id="email" type="email" placeholder="type email"></input>
-        <input id="submit" type="submit"></input>
+      <form onSubmit={handleSubmit}>
+        <input onChange={handleChanges} value={email} id="email" type="email" placeholder="type email"></input>
+        <input  id="submit" type="submit"></input>
       </form>
     </div>
   )
